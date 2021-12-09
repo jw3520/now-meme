e325fe85b94c23ffbbaeed3c37b7b1b612928247
jw3520 <jjw3520@naver.com>
2021-12-09 22:51:18 +0900

    Initial commit

---
+++ src/main/java/kr/memers/nowmeme/NowMemeApplication.java
@@ -0,0 +1,13 @@
+package kr.memers.nowmeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowMemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowMemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/memers/nowmeme/NowMemeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.memers.nowmeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowMemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
